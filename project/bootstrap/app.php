7b34d5b6422acb4f56b8305c1e67d69a2d646010
Ikars Melnalksnis <72831-lyx52@users.noreply.drupalcode.org>
2026-02-03 17:22:07 +0200

    Add middleware for proxy

--- project/bootstrap/app.php
+++ project/bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (ValidationException $exception, Request $request) {
